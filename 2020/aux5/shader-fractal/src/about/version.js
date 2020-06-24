f8610710051180e4efa0f9b70f89d94359e5822d
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-06-24 17:28:25 -0400

    Agrega el código de la auxiliar 5

---
+++ 2020/aux5/shader-fractal/src/about/version.js
@@ -0,0 +1,15 @@
+/**
+ VERSION APP
+ Versión del proyecto.
+
+ @author Pablo Pizarro R. @ppizarror.com
+ @license MIT
+ */
+
+/**
+ * Versión de la aplicación
+ */
+aboutinfo.v = {
+    "date": "26/08/2019",
+    "version": "0.6.0",
+};
